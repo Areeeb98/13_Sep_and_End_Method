73666381e4c401870aeb4f635da7720d4662e7db
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-18 19:57:49 +0530

    Add files via upload

---
+++ Sep_and_End_method.py
@@ -0,0 +1,67 @@
+"""
+Day: Wednesday
+Date: 18th Jan 2023
+Time: 7 PM
+topic: sep method
+       end method
+       combination of sep and end method
+"""
+
+
+''' sep method '''
+
+# print('hello'  ,  'world')
+# print('hello' +' '+  'world')
+# print('hello'+chr(32)+'world')
+
+# print('python'+"-"+'programming'+'-'+'world')
+
+# print('hello', 'world',sep="*")
+# print('p','y','t','h','o','n',sep='@')
+
+# print("hello" , "world")
+# print("hello"  "world")
+# print("hello" +'-'+ "world")
+# print("Hello"  "World",'Core', 'Python',sep="")
+# print("Hello"  "World",'Core', 'Python',sep="-")
+# print("Hello" , "World",'Core', 'Python',sep="-")
+# print("Hello" , "World",'Core', 'Python',sep="-ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" +"ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" ' ' "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-"  "ok-")
+
+''' end method '''
+
+# print("Hello" , "Python",end=".")
+# print("Hello" , "Python")
+# print("New Version is 3.11.1")
+# print("ok fine")
+
+# print("Hello" , "Python",end=".")
+# print("Hello" , "Python",end="-")
+# print("New Version is 3.11.1")
+# print("ok fine")
+
+# print("Hello" , "Python",end=".")
+# print("Hello" , "Python",end="-")
+# print("New Version is 3.11.1",end="@")
+# print("ok fine")
+
+
+''' Combination of sep and end method'''
+
+# print("World" , "Cup" , end="." , sep='') 
+# print("World" , "Cup" , end="." , sep='-') 
+# print("Core" , "Programming" , "Python" , sep="@" , end="#")
+
+# print("Python" *5)
+# print("Python " *5)
+# print("Python\n" *5)
+# print("Python\n" *5, end="")
+
+print("hello", "world", sep=" 123 ")
+
+print('AA\n'*4, end="")
+
+
